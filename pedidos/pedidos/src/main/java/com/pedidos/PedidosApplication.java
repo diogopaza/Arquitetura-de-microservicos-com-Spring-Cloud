9b6f9517519c4c9c8ec121754558f85b5ad2d69b
Diogo Paza <paza@educabiz.com>
2025-05-30 11:01:53 -0300

    atualizando config

--- pedidos/pedidos/src/main/java/com/pedidos/PedidosApplication.java
+++ pedidos/pedidos/src/main/java/com/pedidos/PedidosApplication.java
@@ -2,10 +2,8 @@ package com.pedidos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class PedidosApplication {
 
 	public static void main(String[] args) {
